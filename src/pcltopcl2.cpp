d7ccca13ac381108366ffca619a3cd7b4533ca35
Joey Wang <16601343014@163.com>
2021-11-19 20:05:43 +0800

    oriented point sampling in ros frame

--- src/pcltopcl2.cpp
+++ src/pcltopcl2.cpp
@@ -9,6 +9,13 @@ bool convertPointCloudToPointCloud2(const sensor_msgs::PointCloudConstPtr &cloud
     return false;
 }
 
+void  convertPointCloudToPointCloud2(const sensor_msgs::PointCloudConstPtr &cloudin)
+{
+    sensor_msgs::PointCloud2Ptr PointCloud2(new sensor_msgs::PointCloud2);
+    if (sensor_msgs::convertPointCloudToPointCloud2(*cloudin, *PointCloud2))
+        ROS_INFO("PointCloud are successfully converted to PointCloud2");
+}
+
 bool convertPointCloud2ToPCLXYZ(const sensor_msgs::PointCloud2ConstPtr &cloudin, pcl::PointCloud<pcl::PointXYZ>::Ptr &pclPointcloud)
 {
   pcl::fromROSMsg(*cloudin, *pclPointcloud);
@@ -17,9 +24,9 @@ bool convertPointCloud2ToPCLXYZ(const sensor_msgs::PointCloud2ConstPtr &cloudin,
 
 void convertPointCloudToPCLXYZ(const sensor_msgs::PointCloudConstPtr &cloudin)
 {   
-    sensor_msgs::PointCloud2Ptr PointCloud2;
+    sensor_msgs::PointCloud2Ptr PointCloud2(new sensor_msgs::PointCloud2);
     convertPointCloudToPointCloud2(cloudin, PointCloud2);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr pclPointcloud;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pclPointcloud(new pcl::PointCloud<pcl::PointXYZ>);
     convertPointCloud2ToPCLXYZ(PointCloud2, pclPointcloud);
     ROS_INFO("pclPointCloud converted successfully");
     return;
@@ -31,9 +38,8 @@ int main(int argc, char **argv)
 	ros::NodeHandle n;
     /* define a subscriber named _subCloud, and subscribe topic '/sensor_msg::PointCloud'*/
     ros::Subscriber _subCloud;
-    ros::Publisher _pubCloudPCLXYZ;
-    _subCloud = n.subscribe<sensor_msgs::PointCloud2>
-      ("/radar_pcl", 1, convertPointCloudToPCLXYZ);
+    _subCloud = n.subscribe<sensor_msgs::PointCloud>
+      ("/radar_pcl", 1, &convertPointCloudToPCLXYZ);
     ros::spin();
     return 0;
 }
